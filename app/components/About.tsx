9cd69c4a09670e3b8af75b99b7d9934dae9fa71a
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-11-20 09:32:35 +0100

    add style  About Page

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class About extends React.Component {
   render() {
     return (
       <View>
-        <Text> A propos de moi</Text>
+        <Text style={style.title}> A propos de moi</Text>
         <Text>
-          {" "}
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas autem ipsam velit, corporis cum nam
           incidunt ea nulla natus, perspiciatis nemo rem vel atque magnam! Laborum ad enim quibusdam voluptatibus.
         </Text>
@@ -15,3 +14,10 @@ export default class About extends React.Component {
     );
   }
 }
+
+const style = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    marginBottom: 20,
+  },
+});
