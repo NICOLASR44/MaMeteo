045fe5c0f622efd3cf865757c5b1b38563cffdbf
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-11-20 09:20:36 +0100

    add About Page

---
+++ app/components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text> A propos de moi</Text>
+        <Text>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas autem ipsam velit, corporis cum nam
+          incidunt ea nulla natus, perspiciatis nemo rem vel atque magnam! Laborum ad enim quibusdam voluptatibus.
+        </Text>
+      </View>
+    );
+  }
+}
