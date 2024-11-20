e553b51ed244524ef3e3e3d9659cc670c3e3c6f6
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-11-20 09:55:04 +0100

    add new  style  About Page

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
 export default class About extends React.Component {
   render() {
     return (
-      <View>
+      <View style={style.view}>
         <Text style={style.title}> A propos de moi</Text>
-        <Text>
+        <Text style={style.text}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas autem ipsam velit, corporis cum nam
           incidunt ea nulla natus, perspiciatis nemo rem vel atque magnam! Laborum ad enim quibusdam voluptatibus.
         </Text>
+        <ActivityIndicator style={style.view} color={"#e642f5"} size={"large"} animating={true} />
       </View>
     );
   }
 }
 
 const style = StyleSheet.create({
+  view: {
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   title: {
     fontSize: 22,
     marginBottom: 20,
+  },
+  text: {
+    textAlign: "center",
   },
 });
